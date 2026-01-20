6cfe142a77664cef89c7a57a763de4255a0f56f8
klolage <161434484+klolage@users.noreply.github.com>
2026-01-20 13:06:23 +0530

    Updated SDK version (#6)
    * Updated SDK version

--- Package.swift
+++ Package.swift
@@ -1,5 +1,7 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+//  SDK version: 7.1.1.1
 
 import PackageDescription
 

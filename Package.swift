d5d147bcaf51e2cde0c0c1a8757249d314883d51
klolage <krishna.lolage@microsoft.com>
2026-05-15 12:56:14 +0530

    removed version string from package file

--- Package.swift
+++ Package.swift
@@ -1,7 +1,4 @@
 // swift-tools-version:5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-//
-//  SDK version: 7.1.1.1
 
 import PackageDescription
 
